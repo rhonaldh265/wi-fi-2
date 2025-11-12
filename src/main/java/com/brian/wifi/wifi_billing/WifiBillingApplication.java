5c967c6fe80ae0b915ac85b8502dca5c68bbd4f0
BrianTowett <robivine99@gmail.com>
2025-11-12 20:09:35 +0300

    Initial commit - WiFi Billing System backend

---
+++ src/main/java/com/brian/wifi/wifi_billing/WifiBillingApplication.java
@@ -0,0 +1,13 @@
+package com.brian.wifi.wifi_billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brian/wifi/wifi_billing/WifiBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.wifi.wifi_billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
